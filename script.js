4a8ee6e98922daee1b482ebff4a8f69f4985094b
Elena Tsvetkova <git@etdev.space>
2023-02-22 16:37:29 +0300

    add prototype functionality of adding and deleting tasks

--- script.js
+++ script.js
@@ -54,3 +54,51 @@ class TaskStore {
         }
     }
 }
+
+class App {
+    #ts = new TaskStore();
+
+    constructor() {
+        this.taskList = document.getElementById("taskList");
+        document.getElementById("newTaskField").addEventListener('keydown', this.newTask);
+    }
+
+
+    newListItem(task) {
+        const item = document.createElement('div');
+        item.innerHTML = `<span>${task.title}</span>`
+        item.classList.add('task-item');
+        item.dataset.id = task.id;
+
+        const delBtn = document.createElement('button');
+        delBtn.textContent = 'x';
+        delBtn.onclick = this.removeTask;
+        item.appendChild(delBtn);
+
+        return item;
+    }
+
+
+    newTask = (e) => {
+        if (e.key === 'Enter') {
+            const title = e.target.value;
+            if (title.length !== 0) {
+                const task = this.#ts.add(title);
+                const item = this.newListItem(task);
+                this.taskList.appendChild(item);
+            }
+            e.target.value = '';
+        } else if (e.key === 'Esc') {
+            e.target.blur();
+        }
+    }
+
+    removeTask = (e) => {
+        const id = e.target.parentElement.dataset.id;
+        if (this.#ts.remove(id)) {
+            this.taskList.removeChild(e.target.parentElement);
+        }
+    }
+}
+
+const app = new App();
